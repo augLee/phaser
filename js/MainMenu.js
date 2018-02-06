2b213c181bb11b3413e133c96657701204769a71
augLee <hiom123@naver.com>
2018-02-06 20:37:18 +0900

    loading page

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -1,15 +1,20 @@
+var nameLabel;
+
 var MainMenuState = {
     create:function() {
-        var labelStyle = {font: 'bold 50px Space Mono', fill: '#ffffff', boundsAlignH: "center", boundsAlignV:"middle"};
+        var labelStyle = {font: 'bold 50px Space Mono', fill: '#ffffff', align: "center"};
         
-        var nameLabel = game.add.text(0 ,0,"Click To Start", labelStyle);
-        nameLabel.setShadow(3,3, 'rgba(0,0,0,0,5)', 2);
-        nameLabel.setTextBounds(0,100,800,100);
+        nameLabel = game.add.text(game.world.centerX ,game.world.centerY,"Click To Start", labelStyle);
+        nameLabel.anchor.set(0.5);
+        this.timer = game.time.events.loop(Phaser.Timer.SECOND, this.tick, this);
 
         game.input.activePointer.capture = true;
     },
-
+    tick: function() {
+        if (nameLabel.style)
+    },
     update:function() {
+        //updateLoading(nameLabel);
         if (game.input.activePointer.isDown) {
             game.state.start('play');
         }
--- js/PreLoader.js
+++ js/PreLoader.js
@@ -1,23 +1,44 @@
+
+var tick = 1;
+
+function updateLoading(label) {
+    tick++;
+    if (tick % 2) {
+        console.log(tick);
+        game.add.tween(label).to( {alpha: 0}, 0, Phaser.Easing.Linear.None, true);
+    } else {
+        console.log(tick);
+        game.add.tween(label).to( {alpha: 100}, 0, Phaser.Easing.Linear.None, true);
+    }
+
+}
+
 var PreLoaderState = {
     preload: function() {
-        console.log("width :"+ game.width + " height : " + game.height  );
-        var loadingLabel = game.add.text(game.width / 2,game.height /2, 'loading...', );
-
+        console.log("width :"+ game.width + " height : " + game.height  );       
+        
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         game.scale.pageAlignHorizontally = true;
         game.scale.pageAlignVertically = true;
         game.stage.backgroundColor="#000000";
 
+        var loadingLabel = game.add.text(game.width / 2,game.height /2, 'loading...',
+            {font: 'bold 50px Courier', fill: '#ffffff', align:"center"} );
+        
+        loadingLabel.anchor.set(0.5);
         // load graphics image
         game.load.image("box", "./image/box.png");
         game.load.image("player", "./image/player.png");
         game.load.image("bullet", "./image/bullet.png");
         game.load.image("life", "./image/heart2.png");
         game.load.image("spaceBack", "./image/space.png");
+        game.load.image("loadingMenu", "./image/preLoaderImage.png");
+        game.load.image("MainImage","./image/main.png");
         // load audio assets 
     },
 
     create: function() {
+        
         game.state.start('MainMenu');
     }
 
