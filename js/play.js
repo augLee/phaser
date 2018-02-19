51632ab52e998a5bfffffb8e52710f0b95c32690
augLee <hiom123@naver.com>
2018-02-19 19:09:54 +0900

    stone add

--- js/play.js
+++ js/play.js
@@ -14,6 +14,54 @@ var bulletCnt = 30;
 var bulletMoveVelo = 150;
 
 
+var stone1, stone2, stone3, Alive_stone1, Alive_stone2, Alive_stone3;
+var Arr_Stone1=[];
+var Cnt_Stone1=15;
+var Velo_Stone1=150;
+var Arr_Stone2=[];
+var Cnt_Stone2=15;
+var Velo_Stone2=180;
+var Arr_Stone3=[];
+var Cnt_Stone3=15;
+var Velo_Stone3=200;
+
+
+function kill_stone(k1,k2)
+{
+    if(k2) k2.kill();
+    playerLife--;
+};
+
+function func_overlap(k1,k2, i=0) 
+{
+    game.physics.arcade.overlap(k1, k2, function(k1, k2) {
+        k2.kill();
+        if (i != 0) { 
+            playerLife--;
+            //console.log(playerLife);
+        }
+    }, null, this);    
+
+    if (playerLife < 1) return 1;
+
+    return 0;
+};
+
+function revival_stone(alive, player, stone, stonearray, velo)
+{
+    alive = stone.getFirstExists(false);
+    stonearray.length = 0;
+    
+    box.forEachAlive(function(alive){
+        stonearray.push(alive);
+    });
+    if(alive && stonearray.length > 0) {
+        var Rand = game.rnd.integerInRange(0, stonearray.length -1);
+        var block = stonearray[Rand];
+        alive.reset(block.body.x, block.body.y);
+        game.physics.arcade.moveToObject(alive, player, velo);
+    }
+};
 
 function func_input() {
         var velocity = 200;
@@ -58,6 +106,9 @@ var playState = {
         playerLife = 2;
         score_num = 0;
         bulletMoveVelo = 150;
+        Velo_Stone1 = 150;
+        Velo_Stone2 = 180;
+        Velo_Stone3 = 200;
     },
     preload:function() {
         game.stage.backgroundColor = "#000000";
@@ -88,12 +139,33 @@ var playState = {
 
         inputkey = game.input.keyboard.createCursorKeys();
 
-        bullet = game.add.group();
-        bullet.enableBody = true;
-        bullet.physicsBodyType = Phaser.Physics.ARCADE;
-        bullet.createMultiple(bulletCnt, "bullet");
-        bullet.setAll("outOfBoundsKill", true);
-        bullet.setAll("checkWorldBounds", true);
+        //bullet = game.add.group();
+        //bullet.enableBody = true;
+        //bullet.physicsBodyType = Phaser.Physics.ARCADE;
+        //bullet.createMultiple(bulletCnt, "bullet");
+        //bullet.setAll("outOfBoundsKill", true);
+        //bullet.setAll("checkWorldBounds", true);
+
+        stone1 = game.add.group();
+        stone1.enableBody = true;
+        stone1.physicsBodyType = Phaser.Physics.ARCADE;
+        stone1.createMultiple(Cnt_Stone1, "Stone1");
+        stone1.setAll("outOfBoundsKill", true);
+        stone1.setAll("checkWorldBounds", true);
+
+        stone2 = game.add.group();
+        stone2.enableBody = true;
+        stone2.physicsBodyType = Phaser.Physics.ARCADE;
+        stone2.createMultiple(Cnt_Stone2, "Stone2");
+        stone2.setAll("outOfBoundsKill", true);
+        stone2.setAll("checkWorldBounds", true);
+
+        stone3 = game.add.group();
+        stone3.enableBody = true;
+        stone3.physicsBodyType = Phaser.Physics.ARCADE;
+        stone3.createMultiple(Cnt_Stone3, "Stone3");
+        stone3.setAll("outOfBoundsKill", true);
+        stone3.setAll("checkWorldBounds", true);
 
         bar = game.add.graphics();
         bar.beginFill(0x000000, 0.1);
@@ -115,14 +187,13 @@ var playState = {
 
     update:function() {
         game.physics.arcade.collide(player, box);
-        game.physics.arcade.overlap(bar, bullet, function(bar, bullet) {
-            bullet.kill();
-        }, null, this);
+        func_overlap(bar,stone1);
+        func_overlap(bar,stone2);
+        func_overlap(bar,stone3);
 
-        game.physics.arcade.overlap(player, bullet, function(player,bullet){
-            bullet.kill();
-            playerLife--;
-        }, null, this);
+        var check = func_overlap(player,stone1,1);
+        func_overlap(player,stone2,1);
+        func_overlap(player,stone3,1);
 
         player.body.velocity.setTo(0,0);
 
@@ -136,17 +207,21 @@ var playState = {
         func_input();
         //levelDesign();
 
-        bulletAlive = bullet.getFirstExists(false);
-        bulletArray.length = 0;
-        
-        box.forEachAlive(function(bulletAlive){
-            bulletArray.push(bulletAlive);
-        });
-        if(bulletAlive && bulletArray.length > 0) {
-            var Rand = game.rnd.integerInRange(0, bulletArray.length -1);
-            var bulletBox = bulletArray[Rand];
-            bulletAlive.reset(bulletBox.body.x, bulletBox.body.y);
-            game.physics.arcade.moveToObject(bulletAlive, player, bulletMoveVelo);
-        }
+        revival_stone(Alive_stone1, player,stone1,Arr_Stone1, Velo_Stone1);
+        revival_stone(Alive_stone2, player,stone2,Arr_Stone2, Velo_Stone2);
+        revival_stone(Alive_stone3, player,stone3,Arr_Stone3, Velo_Stone3);
+
+        //bulletAlive = bullet.getFirstExists(false);
+        //bulletArray.length = 0;
+        //
+        //box.forEachAlive(function(bulletAlive){
+        //    bulletArray.push(bulletAlive);
+        //});
+        //if(bulletAlive && bulletArray.length > 0) {
+        //    var Rand = game.rnd.integerInRange(0, bulletArray.length -1);
+        //    var bulletBox = bulletArray[Rand];
+        //    bulletAlive.reset(bulletBox.body.x, bulletBox.body.y);
+        //    game.physics.arcade.moveToObject(bulletAlive, player, bulletMoveVelo);
+        //}
     }
 }
